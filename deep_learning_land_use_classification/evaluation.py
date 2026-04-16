3aaf2fe617479e1d6cacbd116485d7c332543104
TomerP2 <93160956+TomerP2@users.noreply.github.com>
2026-04-16 16:37:55 +0200

    Finished multi_label_ViT

--- deep_learning_land_use_classification/evaluation.py
+++ deep_learning_land_use_classification/evaluation.py
@@ -2,6 +2,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.metrics import hamming_loss
 import torch
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 def compute_accuracy_metrics_singlelabel(model, loader, device):
     model.eval()
@@ -9,8 +10,12 @@ def compute_accuracy_metrics_singlelabel(model, loader, device):
 
     with torch.no_grad():
         for images, labels in loader:
-            images = images.to(device)
-            outputs = model(images)
+            if isinstance(images, dict):
+                images = {k: v.to(device) for k, v in images.items()}
+                outputs = model(images["pixel_values"])
+            else:
+                images = images.to(device)
+                outputs = model(images)
             preds = outputs.argmax(dim=1)
 
             all_preds.append(preds.cpu().numpy())
@@ -36,8 +41,12 @@ def compute_accuracy_metrics_multilabel(model, loader, device, threshold=0.5):
 
     with torch.no_grad():
         for images, labels in loader:
-            images = images.to(device)
-            outputs = model(images)
+            if isinstance(images, dict):
+                images = {k: v.to(device) for k, v in images.items()}
+                outputs = model(images["pixel_values"])
+            else:
+                images = images.to(device)
+                outputs = model(images)
             probs = torch.sigmoid(outputs)
             preds = (probs > threshold).int().cpu().numpy()
 
@@ -56,3 +65,25 @@ def compute_accuracy_metrics_multilabel(model, loader, device, threshold=0.5):
     f1_macro        = f1_score(all_labels, all_preds, average='macro', zero_division=0)
 
     return precision, recall, f1, precision_macro, recall_macro, f1_macro
+
+def get_confusion_matrix_singlelabel(model, loader, device):
+    model.eval()
+    all_labels, all_preds = [], []
+
+    with torch.no_grad():
+        for images, labels in loader:
+            if isinstance(images, dict):
+                images = {k: v.to(device) for k, v in images.items()}
+                outputs = model(images["pixel_values"])
+            else:
+                images = images.to(device)
+                outputs = model(images)
+            preds = outputs.argmax(dim=1)
+
+            all_preds.append(preds.cpu().numpy())
+            all_labels.append(labels.numpy())
+
+    all_preds  = np.concatenate(all_preds)
+    all_labels = np.concatenate(all_labels)
+
+    return confusion_matrix(all_labels, all_preds)
